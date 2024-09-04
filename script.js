d54ebf972fc1c86c20e518741e0afa2f0a98d0d7
JayapriyaS24 <sjayapriya2407@gmail.com>
2024-09-04 08:35:56 -0700

    function added

---
+++ script.js
@@ -0,0 +1,62 @@
+let name=prompt("enter the name")
+console.log(name)
+
+
+alert ("Email is not found")
+
+
+
+let a=Number(prompt("enter the number"))
+console.log(a*b);
+
+let a=10,b=26;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a**b);
+console.log(a%b);
+
+
+let a=10,b=20;
+
+let c=true;
+let d=false;
+
+console.log(a==10&&b==20);
+console.log(a==10||b==20);
+console.log(!c==10);
+
+
+var x=10;   
+var y=20;
+console.log(x+y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
